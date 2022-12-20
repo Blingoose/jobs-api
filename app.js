b5e4c86b583a8dbb33cbe25d21e8432328574584
Andre Katov <andy.katov@icloud.com>
2022-12-20 21:31:46 +0200

    initial boilerplate added

---
+++ app.js
@@ -0,0 +1,38 @@
+import http from "http";
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./db/connect.js";
+import { errorHandlerMiddleware } from "./middleware/error-handler.js";
+import { notFound } from "./middleware/not-found.js";
+
+dotenv.config();
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    const server = express();
+
+    // Application specific middleware
+    server.use(express.json());
+
+    // Route
+    server.get("/", (req, res) => {
+      res.send("jobs api");
+    });
+
+    // Not found middleware
+    server.use(notFound);
+
+    // Error handling middleware
+    server.use(errorHandlerMiddleware);
+
+    const PORT = process.env.PORT || 8000;
+    http.createServer(server).listen(PORT, function () {
+      console.info("Server is listening on:", this.address());
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
