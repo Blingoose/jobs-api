07ad5874342d447ea508e0ac013cd43b96da3b24
Andre Katov <andy.katov@icloud.com>
2023-01-26 13:44:49 +0200

    added password lengthi error handling

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,10 +4,15 @@ import { User } from "../models/User.js";
 import { BadRequest, UnauthenticatedError } from "../errors/index.js";
 
 export const register = asyncWrapper(async (req, res, next) => {
-  // const { name, email, password } = req.body;
-  // if (!name || !email || !password) {
-  //   return next(new BadRequest("Please Provide name, email and password"));
-  // }
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    throw new BadRequest("Please Provide name, email and password");
+  }
+
+  if (req.body.password.length < 6) {
+    throw new BadRequest("Password must be at least 6 characters");
+  }
+
   const user = await User.create({ ...req.body });
   const token = user.createJWT();
   res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
